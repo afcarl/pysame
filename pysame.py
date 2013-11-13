3afa780454c9ed51202d1e9e8f73f59a286e58f1
Yusuke Shinyama <yusuke@shinyama.jp>
2013-11-13 23:07:17 +0900

    Separated Board class.

--- pysame.py
+++ pysame.py
@@ -4,26 +4,16 @@ import pygame
 import random
 
 
-##  PySame
+##  Board
 ##
-class PySame(object):
+class Board(object):
 
-    class Particle(object):
-
-        def __init__(self, surface, rect, count):
-            self.surface = surface
-            self.rect = rect
-            self.count = count
-            return
-
-        def __repr__(self):
-            return '<Particle: %r %r>' % (self.surface, self.rect)
-
-        def update(self):
-            self.count -= 1
-            self.rect.y -= 2
-            return
-            
+    BG_COLOR = (0,0,80)
+    HI_COLOR = (255,255,255)
+    BORDER_COLOR = (0,0,0)
+    BLOCK_COLORS = [ (255,0,0), (0,255,0), (220,220,0),
+                     (0,0,220), (0,200,255) ]
+    
     class Group(object):
 
         def __init__(self, i):
@@ -39,48 +29,34 @@ class PySame(object):
             self.blocks.append(block)
             return
 
-    BG_COLOR = (0,0,80)
-    HI_COLOR = (255,255,255)
-    TEXT_COLOR = (255,255,255)
-    BORDER_COLOR = (0,0,0)
-    BLOCK_COLORS = [ (255,0,0), (0,255,0), (220,220,0),
-                     (0,0,220), (0,200,255) ]
-
-    def __init__(self, blocksize=32):
+    def __init__(self, (bwidth, bheight), blocksize=32):
+        self.bheight = bheight
         self.blocksize = blocksize
-        self.surface = pygame.display.get_surface()
-        self.font = pygame.font.Font('prstartk.ttf', blocksize)
-        self.sounds_remove = [
-            pygame.mixer.Sound('remove1.wav'),
-            pygame.mixer.Sound('remove2.wav'),
-            pygame.mixer.Sound('remove3.wav'),
-            pygame.mixer.Sound('remove4.wav'),
-            ]
+        self._block = []
+        self._focus = None
+        self._selected = None
+        n = len(self.BLOCK_COLORS)
+        for x in xrange(bwidth):
+            self._block.append([ random.randrange(n) for y in xrange(bheight) ])
+        self._update_groups()
         return
 
-    def init_game(self, boardsize=(20,15)):
-        self._score = 0
-        self._init_blocks(boardsize)
-        self._particles = []
-        return
+    def get_bwidth(self):
+        return len(self._block)
 
-    def repaint(self):
-        self.surface.fill(self.BG_COLOR)
-        self._paint_blocks()
-        self._paint_selection()
-        for part in self._particles:
-            self.surface.blit(part.surface, part.rect)
-        text = (u'SCORE: %d' % self._score)
-        self._render_text(text, self.TEXT_COLOR, (0,0))
-        pygame.display.flip()
-        return
+    def get_selection(self):
+        return len(self._selected)
 
-    def update(self):
-        for part in self._particles:
-            part.update()
-        self._particles = [ part for part in self._particles if 0 < part.count ]
-        self.repaint()
-        return
+    def get_selection_rect(self):
+        return self._get_blockrect(self._selected)
+        
+    def render(self):
+        surface = pygame.Surface((self.get_bwidth()*self.blocksize,
+                                  self.bheight*self.blocksize))
+        surface.fill(self.BG_COLOR)
+        self._paint_blocks(surface)
+        self._paint_selection(surface)
+        return surface
 
     def update_selection(self, p):
         self._focus = self._get_blockpos(p)
@@ -92,33 +68,54 @@ class PySame(object):
         return
         
     def remove_selection(self):
-        if not self._selected: return
-        n = len(self._selected)
-        score = n*n
-        self._score += score
-        if 16 <= n:
-            i = 3
-        elif 8 <= n:
-            i = 2
-        elif 4 <= n:
-            i = 1
-        else:
-            i = 0
-        self.sounds_remove[i].play()
-        rect = self._get_blockrect_center(self._selected)
         self._remove_blocks(self._selected)
         self._update_groups()
-        surface = self.font.render(str(n), 1, self.TEXT_COLOR)
-        self._add_particle(surface, rect.center)
         return
 
-    def _init_blocks(self, boardsize):
-        self._block = []
-        (bwidth, self.boardheight) = boardsize
-        n = len(self.BLOCK_COLORS)
+    def _update_groups(self):
+        self._focus = None
+        self._selected = set()
+        # clustering
+        josh = {}
+        i = 0
+        bwidth = self.get_bwidth()
         for x in xrange(bwidth):
-            self._block.append([ random.randrange(n) for y in xrange(self.boardheight) ])
-        self._update_groups()
+            for y in xrange(self.bheight):
+                pos0 = (x,y)
+                block0 = self._block[x][y]
+                if block0 is None: continue
+                if pos0 in josh:
+                    group0 = josh[pos0]
+                    while group0.parent is not None:
+                        group0 = group0.parent
+                else:
+                    group0 = josh[pos0] = self.Group(i)
+                    i += 1
+                neighbours = []
+                if x+1 < bwidth:
+                    neighbours.append( ((x+1,y), self._block[x+1][y]) )
+                if y+1 < self.bheight:
+                    neighbours.append( ((x,y+1), self._block[x][y+1]) )
+                for (pos1,block1) in neighbours:
+                    if block1 is None: continue
+                    if block1 != block0: continue
+                    if pos0 not in josh:
+                        assert 0
+                    elif pos1 not in josh:
+                        josh[pos1] = josh[pos0]
+                    else:
+                        group1 = josh[pos1]
+                        while group1.parent is not None:
+                            group1 = group1.parent
+                        assert group1 is not None
+                        if group1 is not group0:
+                            group1.parent = group0
+        self._groups = {}
+        for (pos,group) in josh.iteritems():
+            while group.parent is not None:
+                group = group.parent
+            group.add(pos)
+            self._groups[pos] = group
         return
 
     def _remove_blocks(self, blocks):
@@ -144,54 +141,126 @@ class PySame(object):
         self._block = [ row for row in self._block if row[0] is not None ]
         return
 
-    def _update_groups(self):
-        self._focus = None
-        self._selected = set()
-        # clustering
-        josh = {}
-        i = 0
-        for x in xrange(len(self._block)):
-            for y in xrange(self.boardheight):
-                pos0 = (x,y)
-                block0 = self._block[x][y]
-                if block0 is None: continue
-                if pos0 in josh:
-                    group0 = josh[pos0]
-                    while group0.parent is not None:
-                        group0 = group0.parent
-                else:
-                    group0 = josh[pos0] = self.Group(i)
-                    i += 1
-                neighbours = []
-                if x+1 < len(self._block):
-                    neighbours.append( ((x+1,y), self._block[x+1][y]) )
-                if y+1 < self.boardheight:
-                    neighbours.append( ((x,y+1), self._block[x][y+1]) )
-                for (pos1,block1) in neighbours:
-                    if block1 is None: continue
-                    if block1 != block0: continue
-                    if pos0 not in josh:
-                        assert 0
-                    elif pos1 not in josh:
-                        josh[pos1] = josh[pos0]
-                    else:
-                        group1 = josh[pos1]
-                        while group1.parent is not None:
-                            group1 = group1.parent
-                        assert group1 is not None
-                        if group1 is not group0:
-                            group1.parent = group0
-        self._groups = {}
-        for (pos,group) in josh.iteritems():
-            while group.parent is not None:
-                group = group.parent
-            group.add(pos)
-            self._groups[pos] = group
-        # ending detection.
-        self._movable = False
-        for group in self._groups.itervalues():
-            if 2 <= len(group.blocks):
-                self._movable = True
+    def _paint_blocks(self, surface):
+        bwidth = self.get_bwidth()
+        lines = []
+        for x in xrange(bwidth):
+            row = self._block[x]
+            for y in xrange(len(row)):
+                block = row[y]
+                if block is None: continue
+                rect = self._get_blockrect([(x,y)])
+                group = self._groups.get((x,y))
+                color = self.BLOCK_COLORS[block]
+                surface.fill(color, rect)
+                if group is not self._groups.get((x+1,y)):
+                    lines.append((rect.topright, rect.bottomright))
+                if group is not self._groups.get((x,y+1)):
+                    lines.append((rect.topleft, rect.topright))
+        for (p1,p2) in lines:
+            pygame.draw.line(surface, self.BORDER_COLOR, p1, p2)
+        return
+
+    def _paint_selection(self, surface):
+        lines = []
+        for (x,y) in self._selected:
+            rect = self._get_blockrect([(x,y)])
+            group = self._groups.get((x,y))
+            if group is not self._groups.get((x-1,y)):
+                lines.append((rect.topleft, rect.bottomleft))
+            if group is not self._groups.get((x+1,y)):
+                lines.append((rect.topright, rect.bottomright))
+            if group is not self._groups.get((x,y-1)):
+                lines.append((rect.bottomleft, rect.bottomright))
+            if group is not self._groups.get((x,y+1)):
+                lines.append((rect.topleft, rect.topright))
+        for (p1,p2) in lines:
+            pygame.draw.line(surface, self.HI_COLOR, p1, p2, 2)
+        return
+        
+    def _get_blockpos(self, (x,y)):
+        bwidth = self.get_bwidth()
+        x = x/self.blocksize
+        y = self.bheight-1-y/self.blocksize
+        if x < 0 or bwidth <= x or y < 0 or self.bheight <= y: return None
+        return (x, y)
+        
+    def _get_blockrect(self, blocks):
+        rect = None
+        for (x,y) in blocks:
+            r = pygame.Rect(x*self.blocksize, (self.bheight-1-y)*self.blocksize,
+                            self.blocksize, self.blocksize)
+            if rect is None:
+                rect = r
+            else:
+                rect.union_ip(r)
+        return rect
+
+    def _get_selected(self, focus):
+        selected = set()
+        if focus in self._groups:
+            group = self._groups[focus]
+            selected.update(group.blocks)
+        return selected
+
+
+##  PySame
+##
+class PySame(object):
+
+    class Particle(object):
+
+        def __init__(self, surface, rect, count):
+            self.surface = surface
+            self.rect = rect
+            self.count = count
+            return
+
+        def __repr__(self):
+            return '<Particle: %r %r>' % (self.surface, self.rect)
+
+        def update(self):
+            self.count -= 1
+            self.rect.y -= 2
+            return
+            
+    BG_COLOR = (0,0,80)
+    TEXT_COLOR = (255,255,255)
+
+    def __init__(self, blocksize=32):
+        self.blocksize = blocksize
+        self.surface = pygame.display.get_surface()
+        self.font = pygame.font.Font('prstartk.ttf', blocksize)
+        self.sounds_remove = [
+            pygame.mixer.Sound('remove1.wav'),
+            pygame.mixer.Sound('remove2.wav'),
+            pygame.mixer.Sound('remove3.wav'),
+            pygame.mixer.Sound('remove4.wav'),
+            ]
+        return
+
+    def init_game(self, boardsize=(20,15)):
+        self._board = Board(boardsize, blocksize=self.blocksize)
+        self._score = 0
+        self._particles = []
+        return
+
+    def repaint(self):
+        self.surface.fill(self.BG_COLOR)
+        board = self._board.render()
+        self.surface.blit(board, (0,0))
+        for part in self._particles:
+            self.surface.blit(part.surface, part.rect)
+        text = (u'SCORE: %d' % self._score)
+        self._render_text(text, self.TEXT_COLOR, (0,0))
+        pygame.display.flip()
+        return
+
+    def update(self):
+        for part in self._particles:
+            part.update()
+        self._particles = [ part for part in self._particles if 0 < part.count ]
+        self.repaint()
         return
 
     def _add_particle(self, surface, (x,y)):
@@ -204,68 +273,6 @@ class PySame(object):
         glyph = self.font.render(text, 1, color)
         self.surface.blit(glyph, pos)
         return
-
-    def _paint_blocks(self):
-        lines = []
-        for x in xrange(len(self._block)):
-            row = self._block[x]
-            for y in xrange(len(row)):
-                block = row[y]
-                if block is None: continue
-                rect = self._get_blockrect((x,y))
-                group = self._groups.get((x,y))
-                color = self.BLOCK_COLORS[block]
-                self.surface.fill(color, rect)
-                if group is not self._groups.get((x+1,y)):
-                    lines.append((rect.topright, rect.bottomright))
-                if group is not self._groups.get((x,y+1)):
-                    lines.append((rect.topleft, rect.topright))
-        for (p1,p2) in lines:
-            pygame.draw.line(self.surface, self.BORDER_COLOR, p1, p2)
-        return
-
-    def _paint_selection(self):
-        lines = []
-        for (x,y) in self._selected:
-            rect = self._get_blockrect((x,y))
-            group = self._groups.get((x,y))
-            if group is not self._groups.get((x-1,y)):
-                lines.append((rect.topleft, rect.bottomleft))
-            if group is not self._groups.get((x+1,y)):
-                lines.append((rect.topright, rect.bottomright))
-            if group is not self._groups.get((x,y-1)):
-                lines.append((rect.bottomleft, rect.bottomright))
-            if group is not self._groups.get((x,y+1)):
-                lines.append((rect.topleft, rect.topright))
-        for (p1,p2) in lines:
-            pygame.draw.line(self.surface, self.HI_COLOR, p1, p2, 2)
-        return
-        
-    def _get_blockpos(self, (x,y)):
-        x = x/self.blocksize
-        y = self.boardheight-1-y/self.blocksize
-        if x < 0 or len(self._block) <= x or y < 0 or self.boardheight <= y: return None
-        return (x, y)
-        
-    def _get_blockrect(self, (x,y)):
-        return pygame.Rect(x*self.blocksize, (self.boardheight-1-y)*self.blocksize,
-                           self.blocksize, self.blocksize)
-
-    def _get_blockrect_center(self, blocks):
-        rect = None
-        for p in blocks:
-            if rect is None:
-                rect = self._get_blockrect(p)
-            else:
-                rect.union_ip(self._get_blockrect(p))
-        return rect
-
-    def _get_selected(self, focus):
-        selected = set()
-        if focus in self._groups:
-            group = self._groups[focus]
-            selected.update(group.blocks)
-        return selected
 
     def run(self, msec=50):
         loop = True
@@ -281,10 +288,25 @@ class PySame(object):
             elif ev.type == pygame.USEREVENT:
                 self.update()
             elif ev.type == pygame.MOUSEMOTION:
-                self.update_selection(ev.pos)
+                self._board.update_selection(ev.pos)
             elif ev.type == pygame.MOUSEBUTTONUP:
-                self.remove_selection()
-                self.update_selection(ev.pos)
+                n = self._board.get_selection()
+                if n:
+                    self._score += n*n
+                    if 16 <= n:
+                        i = 3
+                    elif 8 <= n:
+                        i = 2
+                    elif 4 <= n:
+                        i = 1
+                    else:
+                        i = 0
+                    self.sounds_remove[i].play()
+                    rect = self._board.get_selection_rect()
+                    surface = self.font.render(str(n), 1, self.TEXT_COLOR)
+                    self._add_particle(surface, rect.center)
+                    self._board.remove_selection()
+                self._board.update_selection(ev.pos)
         pygame.time.set_timer(pygame.USEREVENT, 0)
         return
 
